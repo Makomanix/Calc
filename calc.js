75026166d3cdf3f5e34acf86e3ed5007c97d15b9
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2023-11-24 17:53:57 -0500

    event listeners added / displaying numbers / clear and back arrow working

--- calc.js
+++ calc.js
@@ -1,7 +1,63 @@
-const screen = document.querySelector(".screen")
+const screen = document.querySelector('.screen');
+let display = '0'
+let saved = ''
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    updateDisplay();
+};
+
+function handleNumber(number) {
+    if (display === '0') {
+        display = number;
+    } else {
+        display += number;
+    }
+};
+
+function handleSymbol(symbol) {
+    // saved = display;
+
+    switch(symbol) {
+        case "C":
+            display = "0";
+            break;
+
+        case "←":
+            if (display.length === 1) {
+                display = "0"
+            } else {
+                display = display.substring(0, display.length - 1);
+            };
+            break;
+
+        case "=":
+            console.log("equals");
+            break;
+
+        case "+":            
+        case "-":           
+        case "x":           
+        case "÷":
+            console.log("maths");
+            break;
+    }
+};
 
 
+function init() {
+    document.querySelector(".calc-buttons")
+    .addEventListener('click', function(e){
+        buttonClick(e.target.innerText)
+    })
+};
 
-function buttons() {
+function updateDisplay() {
+    screen.innerText = display
+};
 
-}
+init();
