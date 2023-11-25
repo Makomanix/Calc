8d0bcea83fc8d4b29ee4a612a630f68f1b3f648e
Jonathan Bruckman <96599491+Makomanix@users.noreply.github.com>
2023-11-25 14:12:25 -0500

    number/clear buttons functional

--- calc.js
+++ calc.js
@@ -1,99 +1,55 @@
-const screen = document.querySelector('.screen');
-let display = '0'
-let currentTotal = 0;
-let lastOperator;
+let buffer = '0';
 
-function buttonClick(value) {
-    if (isNaN(parseInt(value))) {
+function handleClick(value) {
+    if (isNaN(value)) {
         handleSymbol(value);
     } else {
         handleNumber(value);
-    }
-    updateDisplay();
+    };
 };
 
 function handleNumber(number) {
-    if (display === '0') {
-        display = number;
+    if (buffer === '0') {
+        buffer = number;
     } else {
-        display += number;
-    }
+        buffer += number;
+    };
+
+    rerender();
 };
 
-function handleMath(value) {
-    if (display === "0") {
-        return;
-    }
-
-    const intDisplay = parseInt(display);
-    if (currentTotal === 0) {
-        currentTotal = intDisplay;
-    } else {
-        useOperator(intDisplay)
-    }
-
-    lastOperator = value;
-    display = '0';
-    
-};
-
-function useOperator(intDisplay) {
-    if (lastOperator === '+') {
-        currentTotal += intDisplay
-    } else if (lastOperator === '-') {
-        currentTotal -= intDisplay
-    } else if (lastOperator === "x") {
-      currentTotal *= intDisplay;
-    } else if (lastOperator === "÷") {
-        currentTotal /= intDisplay;
-    }
-    
-}
 
 function handleSymbol(symbol) {
-
-    switch(symbol) {
+    switch (symbol) {
         case "C":
-            display = "0";
+            buffer = "0"
+            rerender();
             break;
-
         case "←":
-            if (display.length === 1) {
-                display = "0"
-            } else {
-                display = display.substring(0, display.length - 1);
-            };
             break;
-
         case "=":
-            if (lastOperator === null) {
-                return;
-            }
-            useOperator(parseInt(display));
-            lastOperator = null;
-            display = "" + currentTotal;
-            currentTotal = 0;
             break;
-
-        case "+":            
-        case "-":           
-        case "x":           
+        case "+":
+        case "-":
+        case "x":
         case "÷":
-            handleMath(symbol);
             break;
     }
+    
 };
 
-
 function init() {
-    document.querySelector(".calc-buttons")
-    .addEventListener('click', function(e){
-        buttonClick(e.target.innerText)
+    document
+    .querySelector('.calc-buttons')
+    .addEventListener('click', function(e) {
+        handleClick(e.target.innerText)
     })
 };
 
-function updateDisplay() {
-    screen.innerText = display
+function rerender() {
+    document
+    .querySelector('.screen')
+    .innerText = buffer
 };
 
 init();
